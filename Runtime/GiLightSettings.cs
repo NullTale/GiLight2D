0ae0a09a50b1867577a58f4373a460513f6edd2f
NullTale <nulltale@gmail.com>
2023-08-17 15:58:54 +0300

    Set volume default scale to one

--- Runtime/GiLightSettings.cs
+++ Runtime/GiLightSettings.cs
@@ -16,7 +16,7 @@ namespace GiLight2D
         public ClampedFloatParameter m_Scale      = new ClampedFloatParameter(1, 0, 2);
         public NoiseTextureParameter m_NoiseTex   = new NoiseTextureParameter(NoiseTexture.Random);
         public Vector2Parameter      m_NoiseVel   = new Vector2Parameter(new Vector2(0, 0));
-        public ClampedFloatParameter m_NoiseScale = new ClampedFloatParameter(0, 0, 1);
+        public ClampedFloatParameter m_NoiseScale = new ClampedFloatParameter(1, 0, 1);
         public ClampedFloatParameter m_Blur       = new ClampedFloatParameter(0, 0, 1);
         
         // =======================================================================
